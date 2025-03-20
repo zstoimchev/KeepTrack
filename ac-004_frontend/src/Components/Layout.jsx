363040a5f63bf50f774f27506ba0e00bc2f69c47
Zhivko <zivkostoimcev1551@outlook.com>
2025-03-20 02:38:48 +0100

    [FIX] layout is 50/50 done, now I move to components (side/nav bar)

--- ac-004_frontend/src/Components/Layout.jsx
+++ ac-004_frontend/src/Components/Layout.jsx
@@ -1,18 +1,24 @@
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import './Layout.css'
 
-function Layout({ children }) {
-    return (
-        <div style={{   height: '100vh', width: '100%' }}>
-            <Navbar />
-            {/*<div style={{ display: 'flex', flex: 1 }}>*/}
-            {/*    <Sidebar />*/}
-            {/*    <main style={{ flex: 1, padding: '20px' }}>*/}
-            {/*        {children}*/}
-            {/*    </main>*/}
-            {/*</div>*/}
+
+function Layout({children}) {
+    return (<div className="ContainerColumn">
+        <div className="AppClass">
+            <h1>test</h1>
+            <Sidebar />
         </div>
-    );
+
+        <div className="AppClass RightColumn">
+            <div className="Navbar">
+                <Navbar/>
+            </div>
+            <div className="DynamicContent">
+                {children}
+            </div>
+        </div>
+    </div>);
 }
 
 export default Layout;
