9b7c29b0c5d139dcf37187703e8a46d78a41b3d3
Hristijan Chupetreski <89211097@student.upr.si>
2025-03-23 16:04:19 +0000

    main page update

--- ac-004_frontend/src/Components/Layout.jsx
+++ ac-004_frontend/src/Components/Layout.jsx
@@ -2,23 +2,30 @@ import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import './Layout.css'
 
-function Layout({ children }) {
+
+function Layout({children}) {
     return (
-      <div className="ContainerColumn">
+    <div className="ContainerColumn">
+        
         <div className="AppClass">
-          {/* Sidebar */}
-          <Sidebar />
+            {/*<h1>test</h1>*/}
+            <Sidebar />
         </div>
-  
-        <div className="AppClass RightColumn">
-          <div className="Navbar">
-            <Navbar />
-          </div>
-          <div className="DynamicContent">
-            {children}
-          </div>
+
+        <div className="RightColumn">
+            <div className="Navbar">
+                <Navbar/>
+            </div>
+
+            <div className="Dynamic">
+                <div className="Calendar">
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FBelgrade&showPrint=0&src=Y3VwZXRyZWtpa29AZ21haWwuY29t&src=ZW4uc2xvdmVuaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=bnl0aW1lcy5jb21fODlhaTRpanBiNzMzZ3QyOHJnMjFkMmMyZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%237986CB&color=%230B8043&color=%23E4C441"></iframe>
+                </div>
+            </div>
+
         </div>
-      </div>
-    );
-  }
+
+    </div>);
+}
+
 export default Layout;
--- ac-004_frontend/src/Components/Sidebar.jsx
+++ ac-004_frontend/src/Components/Sidebar.jsx
@@ -2,27 +2,37 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-    return (<div className="sidebar">
+    return (
+    <div className="sidebar">
+
         <div className="user-section">
             <div className="user-icon"></div>
             <p>User</p>
         </div>
+
         <div className="tasks-section">
             <p>Long-Term Tasks</p>
             {[...Array(4)].map((_, i) => (<div key={i} className="task">
                 TIME | TASK {i + 1}
             </div>))}
-            <div className="add-task">+ Add new</div>
+            <div  className="button-section">
+                <p>+ Add new</p>
+            </div>
+        
         </div>
+
+        
         <div className="date-section">
             <p>02</p>
             <p>JULY</p>
             <p>2021</p>
         </div>
-        <div className="options-section">
+
+        <div className="button-section">
             <p>SETTINGS</p>
             <p>LOG OUT</p>
         </div>
+
     </div>);
 };
 
