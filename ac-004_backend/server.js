ececf78da31c638d0fcbf7f86559f57a60a98b55
Zhivko <zivkostoimcev1551@outlook.com>
2025-03-28 14:06:02 +0100

    [UPDATE] ability to serve static files for frontend

--- ac-004_backend/server.js
+++ ac-004_backend/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const cors = require("cors")
+const path = require("path")
 const PORT = process.env.PORT || 3000
 const app = express()
 const db = require('./DB/queries')
 
 app.use(express.json());
 app.use(cors({
-    methods: ['GET', 'POST', 'DELETE', 'PUT'],
-    credentials: true,
-    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'DELETE', 'PUT'], credentials: true, origin: ['http://localhost:5173'],
 }))
 
 const users = require("./routes/users")
@@ -16,11 +15,12 @@ const tasks = require("./routes/tasks")
 app.use('/users', users)
 app.use('/tasks', tasks)
 
+app.use(express.static(path.join(__dirname, "dist")))
 
-app.get('/', (req, res) => {
-    res.json({success: true, msg: "it is working dzonii"})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
