fe8c6032dd5f2617a82a4012be5750121fac9be1
Zhivko <zivkostoimcev1551@outlook.com>
2025-03-29 18:19:36 +0100

    [FIX] commented out static route, need for developmentgit add ac-004_backend/server.js !

--- ac-004_backend/server.js
+++ ac-004_backend/server.js
@@ -15,11 +15,11 @@ const tasks = require("./routes/tasks")
 app.use('/users', users)
 app.use('/tasks', tasks)
 
-app.use(express.static(path.join(__dirname, "dist")))
+// app.use(express.static(path.join(__dirname, "dist")))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "dist", "index.html"))
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
