7571a30b98e77ff5d06449134cbcea343762cc53
Zhivko <zivkostoimcev1551@outlook.com>
2025-04-02 00:12:32 +0200

    [ADD/UPDATE] connected firebase with sample route/collection

--- ac-004_backend/server.js
+++ ac-004_backend/server.js
@@ -1,26 +1,17 @@
-const express = require('express')
-const cors = require("cors")
-const path = require("path")
-const PORT = process.env.PORT || 3000
-const app = express()
-const db = require('./DB/queries')
+import express from 'express';
+import cors from 'cors';
 
+import config from './config.js';
+import productRoute from './routes/productRoute.js';
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.use(cors({
-    methods: ['GET', 'POST', 'DELETE', 'PUT'], credentials: true, origin: ['http://localhost:5173'],
-}))
 
-const users = require("./routes/users")
-const tasks = require("./routes/tasks")
-app.use('/users', users)
-app.use('/tasks', tasks)
+//routes
+app.use('/api', productRoute);
 
-// app.use(express.static(path.join(__dirname, "dist")))
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "dist", "index.html"))
-// })
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(config.port, () =>
+    console.log(`Server is live @ ${config.hostUrl}`),
+);
